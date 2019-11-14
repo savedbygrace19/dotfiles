a0228c0ca6403d9b4d9ef6f4f87b86e4e813ca59
Kevin Peters <kwpeters@ra.rockwell.com>
2019-11-14 11:57:28 -0500

    Added Bash configuration for Windows.

--- bash_profile_win.bash
+++ bash_profile_win.bash
@@ -4,3 +4,26 @@ source ~/dev/dotfiles/run-ssh-agent.bash
 
 alias ls='ls -lGFA --color'
 alias lsd='ls -lGFA --color | grep -i "^d.*/"'
+
+
+#-------------------------------------------------------------------------------
+# Misc Aliases
+#-------------------------------------------------------------------------------
+alias h="history"
+alias e="$EDITOR"
+
+
+#-------------------------------------------------------------------------------
+#
+# PATH
+#
+#-------------------------------------------------------------------------------
+export PATH=${PATH}:~/dev/juggernaut
+export PATH=${PATH}:~/dev/juggernautJS/src
+export PATH=${PATH}:~/dev/juggernautBash
+alias findgrep="findgrep.sh"
+
+export PATH=${PATH}:~/dev/stella/dist/app
+
+
+
