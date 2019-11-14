1695d6d95aad297373f55edea8a938c17d5fa3f5
Kevin Peters <kwpeters@ra.rockwell.com>
2019-11-14 08:17:15 -0500

    Added Windows ssh-agent execution.
    ssh-agent is now started whenever a Bash window is opened on Windows.

--- bash_profile_win.bash
+++ bash_profile_win.bash
@@ -1,5 +1,6 @@
 echo 'Performing Windows initialization (bash_profile_win.bash)...'
 
+source ~/dev/dotfiles/run-ssh-agent.bash
 
 alias ls='ls -lGFA --color'
 alias lsd='ls -lGFA --color | grep -i "^d.*/"'
