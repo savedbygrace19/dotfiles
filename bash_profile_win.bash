a1f353ed0c02351501ac25de25b8cceb642af08e
Kevin Peters <kwpeters@ra.rockwell.com>
2019-11-15 10:01:54 -0500

    Set Windows PS1 prompt.

--- bash_profile_win.bash
+++ bash_profile_win.bash
@@ -2,13 +2,18 @@ echo 'Performing Windows initialization (bash_profile_win.bash)...'
 
 source ~/dev/dotfiles/run-ssh-agent.bash
 
-alias ls='ls -lGFA --color'
-alias lsd='ls -lGFA --color | grep -i "^d.*/"'
+#-------------------------------------------------------------------------------
+# Setup the prompt
+#-------------------------------------------------------------------------------
+parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' ; }
+export PS1="\e[0;34m\w\e[m\e[0;32m\$(parse_git_branch)\e[m \$ "
 
 
 #-------------------------------------------------------------------------------
 # Misc Aliases
 #-------------------------------------------------------------------------------
+alias ls='ls -lGFA --color'
+alias lsd='ls -lGFA --color | grep -i "^d.*/"'
 alias h="history"
 alias e="$EDITOR"
 
@@ -24,6 +29,3 @@ export PATH=${PATH}:~/dev/juggernautBash
 alias findgrep="findgrep.sh"
 
 export PATH=${PATH}:~/dev/stella/dist/app
-
-
-
