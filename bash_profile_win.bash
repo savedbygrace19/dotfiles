1f8fea4405ca8c87fc1904d94a73be7ca10a2709
Kevin Peters <kwpeters@ra.rockwell.com>
2019-12-06 08:46:42 -0500

    Improved prompt on Windows.

--- bash_profile_win.bash
+++ bash_profile_win.bash
@@ -7,7 +7,8 @@ source ~/dev/kwp/dotfiles/run-ssh-agent.bash
 #-------------------------------------------------------------------------------
 parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' ; }
 export PS1="\e[0;34m\w\e[m\e[0;32m\$(parse_git_branch)\e[m \$ "
-
+# Add some whitespace before each prompt line for better readability.
+PS1="\n$PS1"
 
 #-------------------------------------------------------------------------------
 # Misc Aliases
