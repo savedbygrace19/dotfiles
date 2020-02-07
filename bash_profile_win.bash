04bed1b627424a126753655f33e42c8a412ebe54
Kevin Peters <kwpeters@ra.rockwell.com>
2020-02-07 10:51:55 -0500

    Removed windows prompt customization due to weird observed behavior.

--- bash_profile_win.bash
+++ bash_profile_win.bash
@@ -5,10 +5,10 @@ source ~/dev/kwp/dotfiles/run-ssh-agent.bash
 #-------------------------------------------------------------------------------
 # Setup the prompt
 #-------------------------------------------------------------------------------
-parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' ; }
-export PS1="\e[0;34m\w\e[m\e[0;32m\$(parse_git_branch)\e[m \$ "
+# parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' ; }
+# export PS1="\e[0;34m\w\e[m\e[0;32m\$(parse_git_branch)\e[m \$ "
 # Add some whitespace before each prompt line for better readability.
-PS1="\n$PS1"
+# PS1="\n$PS1"
 
 #-------------------------------------------------------------------------------
 # Misc Aliases
