2e6819f8437af521ffe76425ed3ef24bdfc8afc4
Kevin Peters <kwpeters@ra.rockwell.com>
2021-02-12 21:47:48 -0500

    Updated powershell profile.

--- powershell_7/Microsoft.PowerShell_profile.ps1
+++ powershell_7/Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,10 @@
 # Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
 #
 
+
+################################################################################
+# Posh Git
+################################################################################
 Import-Module posh-git
 
 # In the prompt, replace the home directory path with "~".
@@ -18,6 +22,29 @@ $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 $GitPromptSettings.DefaultPromptPath.ForegroundColor = 'Orange'
 # Insert a newline before the prompt suffix.
 $GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
+
+
+################################################################################
+# Elevating to admin
+################################################################################
+function Enter-AdminPSSession {
+  if ($env:OS -eq 'Windows_NT') {
+    Start-Process -Verb RunAs (Get-Process -Id $PID).Path
+  } else {
+    sudo (Get-Process -Id $PID).Path
+  }
+}
+
+# Optionally also define a short alias name:
+# Note: 'psadmin' is a nonstandard alias name; a more conformant name would be
+#       the somewhat clunky 'etasn'
+#       ('et' for 'Enter', 'a' for admin, and 'sn'` for session)
+Set-Alias psadmin Enter-AdminPSSession
+
+
+################################################################################
+# PATH environment variable
+################################################################################
 
 # Append script location(s) to the end of the PATH environment variable.
 $env:path += ";C:\Users\kwpeters.RA-INT\dev\kwp\PowerShellScripts"
